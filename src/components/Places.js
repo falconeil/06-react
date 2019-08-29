0ae47fe2d8fe062b6e41f76ea6e07e01b19b0cbb
Nil <nilarmengol@gmail.com>
2019-08-29 15:05:52 +0700

    060202

--- src/components/Places.js
+++ src/components/Places.js
@@ -11,8 +11,10 @@ state = {
 render() {
 	return (
 
-		<div>{this.state.title}</div>,
-		<p>{this.state.title} {this.state.title.length}</p>
+
+
+		<h1>{this.state.places.length}</div>,
+		<p>{this.state.title} </p>
 		{
 		this.state.places.map((p,i) => <Places key={i}/>)
 	}
