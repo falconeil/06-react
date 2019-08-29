eae0ae657f87a349b8eda205abe65eddd610fbdb
Nil <nilarmengol@gmail.com>
2019-08-29 16:20:04 +0700

    060203

--- src/components/Places.js
+++ src/components/Places.js
@@ -9,19 +9,16 @@ places: [
 	{title: 'Transpotting', price: 1973 , location: 'Scotland'},
 	{title: 'Adele', price: 2016 , location: 'Paris'},
 	{title: 'Taxi', price: 2006 , location: 'Teheran'}
-//
-// }
+}
+
 render() {
 	return (
-<h1> hi</h1>
-	// 	<h1>{this.state.places.length} Places</div>,
-	// 	<div className='thumbnails'>{this.state.places.map((p,i) =>{
-	// 		p {this.state.places.title} {this.state.places.price}
-	// 	} </div>
-	//
-	// )
-)
+
+		<h1>{this.state.places.length} Places</h1>,
+		<div className='thumbnails'>{this.state.places.map(p =>{
+			<div className='thumbnails'>{p}</div>
+		})</div>
 	}
-// }
-//
+}
+
 export default Places
