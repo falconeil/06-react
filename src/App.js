d22fcaf6cbd1152cef7ab3cf07cc2df3cb417c17
Nil <nilarmengol@gmail.com>
2019-08-29 15:24:14 +0700

    060301

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Places from './components/Places'
 
 class App extends React.Component {
 	render() {
 		return(<h1>Hello World</h1>)
+
 	}
 }
 
--- src/components/Places.js
+++ src/components/Places.js
@@ -2,21 +2,22 @@ import Places from 'react'
 
 class App extends React.Component {
 
-state = {
-	places: ['1', '2', '3', '4', '5'],
-	title: ['Barcelona', 'Dublin', 'London', 'Tokyo', 'Sofia'],
-	price: ['$200', '$170', '$340', '$670', '$57']
-
-}
+// state = {
+// 	places: ['1', '2', '3', '4', '5'],
+// 	title: ['Barcelona', 'Dublin', 'London', 'Tokyo', 'Sofia'],
+// 	price: ['$200', '$170', '$340', '$670', '$57']
+//
+// }
 render() {
 	return (
-
-		<h1>{this.state.places.length} Places</div>,
-		<div className='thumbnails'>{this.state.places.map((p,i) =>{
-			p {this.state.places.title} {this.state.places.price}
-		} </div>
-
-	)
+<h1> hi</h1>
+	// 	<h1>{this.state.places.length} Places</div>,
+	// 	<div className='thumbnails'>{this.state.places.map((p,i) =>{
+	// 		p {this.state.places.title} {this.state.places.price}
+	// 	} </div>
+	//
+	// )
+)
 	}
 }
 
