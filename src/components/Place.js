e68b9aa9a9477a1452b398c3cd981df527fd0a40
Nil <nilarmengol@gmail.com>
2019-09-03 10:48:08 +0700

    060802

---
+++ src/components/Place.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Place extends React.Component {
+
+
+	render() {
+
+		return(
+
+		<h1>here is a place</h1>
+		)
+	}
+}
+
+export default Place
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Places from './Places'
+import Place from './Place'
+import {Link} from 'react-router-dom'
 
 
 class Routes extends React.Component {
 	render () {
   return (
     <BrowserRouter>
+			<Link to="/">Places</Link>
+			<Link to="/Place">Place</Link>
       <Switch>
-
-         <Route path='/' component={Places} />
+  			<Route path='/Place' component={Place} />
+        <Route path='/' component={Places} />
 
       </Switch>
     </BrowserRouter>
   )
 }
 }
+
+
+
 
 export default Routes
