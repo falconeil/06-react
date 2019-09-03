adb47e2f8c60d51c14fc0db4a3f95919471e725b
Nil <nilarmengol@gmail.com>
2019-09-03 14:11:37 +0700

    trying to solve last exercise

--- src/components/Place.js
+++ src/components/Place.js
@@ -9,9 +9,24 @@ class Place extends React.Component {
 	'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg',
 	'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg',
 	'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg'],
-
+	selectedImage: ''
 	}
+componentWillMount(){
+	this.setState({
+		selectedImage: this.state.images[0]
+	})
+}
 
+
+changePic = (i) => {
+
+this.setState({
+	selectedImage: this.state.images[i]
+})
+
+
+
+}
 
 	render() {
 
@@ -19,21 +34,17 @@ class Place extends React.Component {
 
 
 return(
-<div>
-<div style= {{backgroundImage: 'url('+this.state.images[0]+')', height: '600px', backgroundRepeat: 'no-repeat',  width: '850px', 	backgroundSize: 'cover',
-	backgroundPosition: 'center'
-}} ></div>
-<div style= {{backgroundImage: 'url('+this.state.images[1]+')', height: '100px', backgroundRepeat: 'no-repeat',  width: '100px', 	backgroundSize: 'cover',
-	backgroundPosition: 'center'
-}} ></div>
-<div style= {{backgroundImage: 'url('+this.state.images[2]+')', height: '100px', backgroundRepeat: 'no-repeat',  width: '100px', 	backgroundSize: 'cover',
-	backgroundPosition: 'center'
-}} ></div>
-<div style= {{backgroundImage: 'url('+this.state.images[3]+')', height: '100px', backgroundRepeat: 'no-repeat',  width: '100px', 	backgroundSize: 'cover',
-	backgroundPosition: 'center'
-}} ></div>
+	<div>
 
+		<div style= {{backgroundImage: 'url('+this.state.images[0]+')', height: '600px', backgroundRepeat: 'no-repeat',  width: '850px', 	backgroundSize: 'cover',
+		backgroundPosition: 'center'
+	}} ></div>
 
+	{this.state.images.map((e,i) =>{
+		return <div onClick={() => { this.changePic(i)}} style= {{backgroundImage: 'url('+this.state.images[i]+')', height: '100px', backgroundRepeat: 'no-repeat',  width: '150px', 	backgroundSize: 'cover',
+		backgroundPosition: 'center'
+	}} ></div>
+})}
 
 </div>
 
